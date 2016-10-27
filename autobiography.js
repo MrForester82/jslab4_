233fed92777c20d4316e8534230b1cd9ee452331
MrForester82 <potate82@ya.ru>
2016-10-27 14:49:38 +0300

    Создание файла autobiography.js

---
+++ autobiography.js
@@ -0,0 +1,31 @@
+<script>
+
+/*function autob(surname, name, lastname, date_of_birth, place_of_birth, favourite_activity, not_favourite_activity){
+	win = window.open("new_page.html", "Биография");
+}*/
+
+function aut(win){
+            win.document.open();
+            win.document.write("<!DOCTYPE html>");
+            win.document.write("<html lang='en'>");
+            win.document.write("<head>");
+            win.document.write("<meta charset='utf-8'>");
+            win.document.write("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
+            win.document.write("<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>");
+            win.document.write("<link rel='icon' href='image\\favicon.png'>");
+            win.document.write("<title>jslab4</title>");
+            win.document.write("<link href='bootstrap.min.css' rel='stylesheet'>");
+            win.document.write("<link href='jumbotron.css' rel='stylesheet'>");
+            win.document.write("<link href='grid.css' rel='stylesheet'>");
+            win.document.write("<link href='signin.css' rel='stylesheet'>");
+            win.document.write("</head>");
+            win.document.write("<body>");
+            win.document.write("<nav class='navbar navbar-static-top navbar-dark bg-inverse'>");
+            win.document.write("<a class='navbar-brand' href='#'>jslab4</a>");
+            win.document.write("</nav>");
+            win.document.write("hi");
+            
+
+        }
+
+</script>
